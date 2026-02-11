bf690839143f58a158e8c18d712e4aa14ea06aa5
alberto.395919 <alberto.395919@balagtas.sti.edu.ph>
2026-02-11 20:23:25 +0800

    feat : added Division class.

---
+++ Alberto_Github_Calculator_Sample/Division.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alberto_Github_Calculator_Sample
+{
+    public class Division
+    {
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return num1 / num2;
+        }
+    }
+}
